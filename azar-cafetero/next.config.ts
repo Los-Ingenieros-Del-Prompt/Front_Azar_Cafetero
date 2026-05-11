3364711a22b51a16dd0758f807755ed7eac6d272
Karol2905 <karol.estupinan-v@mail.escuelaing.edu.co>
2026-05-11 18:53:49 -0500

    feat: add proxy

--- azar-cafetero/next.config.ts
+++ azar-cafetero/next.config.ts
@@ -1,23 +1,15 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  turbopack: {
-    root: path.resolve(__dirname),
-  },
-};
-
-
-module.exports = {
   async rewrites() {
     return [
       {
         source: '/api-proxy/:path*',
         destination: 'http://azar-alb-774975018.us-east-1.elb.amazonaws.com/:path*',
       },
-    ]
+    ];
   },
-}
+};
 
 export default nextConfig;
